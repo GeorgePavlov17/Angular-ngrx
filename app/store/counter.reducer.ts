9befd58972def33bac005d4f9c3054582006dd57
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-22 15:07:58 +0300

    exploring Selectors and adding doubleCounter Selector

--- app/store/counter.reducer.ts
+++ app/store/counter.reducer.ts
@@ -1,14 +1,17 @@
-import { createReducer, on } from "@ngrx/store";
-import { decrement, increment } from "./counter.actions";
+import { Action, createReducer, on } from "@ngrx/store";
+import {  increment, decrement } from "./counter.actions";
 
 const initialState = 0;
 
 export const counterReducer = createReducer(
     initialState,
     on(increment, (state, action) => state + action.value),
-    on(decrement, (state) => state - 1)
+    on(decrement, (state, action) => state - action.value)
 );
 
-// export function counterReducer(state = initialState) {
+// export function counterReducer(state = initialState, action: CounterActions | Action) {
+//     if(action.type === INCREMENT) {
+//         return state + (action as IncrementAction).value;
+//     }
 //     return state;
 // }

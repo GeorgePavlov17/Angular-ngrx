c61efee13b2ad3b5bd9fc3cd99fcc561adaeee22
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-22 16:02:56 +0300

    adding a Second Effect and get it from localStorage

--- app/store/counter.reducer.ts
+++ app/store/counter.reducer.ts
@@ -1,12 +1,14 @@
 import { Action, createReducer, on } from "@ngrx/store";
-import {  increment, decrement } from "./counter.actions";
+import {  increment, decrement, set } from "./counter.actions";
 
 const initialState = 0;
 
 export const counterReducer = createReducer(
     initialState,
     on(increment, (state, action) => state + action.value),
-    on(decrement, (state, action) => state - action.value)
+    on(decrement, (state, action) => state - action.value),
+    on(set, (state, action) => action.value),
+    
 );
 
 // export function counterReducer(state = initialState, action: CounterActions | Action) {
